a5ce065412ac204e0ee58a4c9e2282053514b1eb
danielNewaz <danielnwz420@gmail.com>
2022-06-30 21:02:49 -0700

    dded

--- index.js
+++ index.js
@@ -16,20 +16,60 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function  run (){
     try{
         await client.connect();
-        const taskCollection = client.db('job_task').collection('task');
+        const tasksToDoCollection = client
+            .db("TaskManagerDB")
+            .collection("Tasks_TODO");
 
-        app.post('/task', async (req, res) => {
-            const task = req.body;
-            const result = await taskCollection.insertOne(task);
-            res.send(result);
-        })
+            app.post("/add-task", async(req, res) => {
+                const { TaskTitle } = req.body;
+                let task = {
+                    TaskTitle: TaskTitle,
+                    isComplete: false,
+                };
+                const id = (await tasksToDoCollection.insertOne(task)).insertedId;
+                if (id) {
+                    res.send({ success: true, msg: "Task added successfully" });
+                }
+            });
 
-        app.get('/task', async (req, res) => {
-            const query = { };
-            const cursor = taskCollection.find(query)
-            const task = await cursor.toArray();
-            res.send(task);
-        })
+            app.patch("/task-complete/:id", async(req, res) => {
+                const id = req.params.id;
+                // const query = { _id: ObjectId(id) };
+                console.log(id);
+    
+                const filter = { _id: ObjectId(id) };
+                const updateTask = {
+                    $set: {
+                        isComplete: true,
+                    },
+                };
+                const result = await tasksToDoCollection.updateOne(filter, updateTask);
+                console.log(result);
+                res.send(result);
+            });
+            
+            app.patch("/edit-task/:id", async(req, res) => {
+                const id = req.params.id;
+    
+                const { TaskTitle } = req.body;
+    
+                const filter = { _id: ObjectId(id) };
+                const updateTask = {
+                    $set: {
+                        isComplete: true,
+                    },
+                };
+                const result = await tasksToDoCollection.updateOne(filter, updateTask);
+                console.log(result);
+                res.send(result);
+            });
+    
+            app.get("/getTasksToDo", async(req, res) => {
+                const query = {};
+                const cursor = tasksToDoCollection.find(query);
+                const tasks = await cursor.toArray();
+                res.send(tasks);
+            });
     }
     finally{
 
